514a779cf66904e05e36658d2e3ee1e082cfb6a5
chipans <jorgchipans@gmail.com>
2025-09-09 12:53:42 -0400

    Modificaion para consulta API-postman

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
+})->middleware('auth:sanctum'); // <-- Fíjate en la sintaxis correcta aquí
+
+Route::get('/hola', function(Request $request) {
+    return 'Hola mundo';
 });
